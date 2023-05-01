c4039d68115a6c578846dab9b9aae1fe80573732
sleepy0owl <souravcristiano502@gmail.com>
2023-05-01 12:18:13 +0530

    Remove debug lines

--- src/utils.ts
+++ src/utils.ts
@@ -3,13 +3,8 @@ import * as core from '@actions/core'
 export function get_databricks_host(): string {
     const databricks_host_input: string = core.getInput('databricks-host')
     const databricks_host_env: string = process.env['DATABRICKS_HOST'] || ''
-    core.debug(`DATABRICKS HOST INPUT :: ${databricks_host_input}`)
-    core.debug(`DATABRICKS HOST ENV :: ${databricks_host_env}`)
 
     if (!databricks_host_input && !databricks_host_env) {
-        core.debug(
-            'databricks-host or DATABRICKS_HOST environment variable must be set.'
-        )
         throw new Error(
             'databricks-host or DATABRICKS_HOST environment variable must be set.'
         )
